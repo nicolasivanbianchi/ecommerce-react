9f29010e109e42ef44371523fa1cedc45aec5433
nikzen <nicolasivanbianchi@outlook.com>
2022-09-01 22:51:41 -0300

    several fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar"
+import ItemListContainer from './components/NavBar/ItemListContainer'
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <ItemListContainer/>
     </div>
   );
 }
---
+++ src/components/NavBar/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+
+export function ItemListContainer (){
+    return(
+    <h1>Loading...</h1>
+    )
+}
+
+export default ItemListContainer
